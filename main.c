b276fab31aae652909f81235138cbea42d0967e2
elhakmaouy97 <maryamelhakmaouy@gmail.com>
2025-09-05 23:40:53 +0100

    les-fontions

---
+++ main.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*int somme(int x,int y){                 //Challenge 1 : Fonction de Somme
+
+    int s;
+s=(x+y);
+return s;
+
+}
+int main()
+{
+int a,b,resultat;
+   printf("veuillez saisir la valeur de a:");
+   scanf("%d",&a);
+   printf("veuillez saisir la valeur de b:");
+   scanf("%d",&b);
+   resultat=somme(a,b);
+   printf("la somme des entier est %d",resultat);
+return 0;
+}*/
+
+
+/*void produit(int x,int y){                //Challenge 2 : Fonction de Multiplication
+int p;
+p=x*y;
+printf("le produit des deux entier saisi est :%d",p);
+
+}
+
+int main(){
+
+int a,b;
+printf("saisir la valeur de a :");
+scanf("%d",&a);
+printf("saisir la valeur de b :");
+scanf("%d",&b);
+produit(a,b);
+   return 0;
+}*/
+/*void maximum(int x, int y){                    //Challenge 3 : Fonction de Maximum
+
+ int max;
+
+ if (x>y){
+        max=x;
+    printf("le maximum entre ces deux entier est :\n%d",max);}
+else if (x<y){
+    max=y;
+    printf("le maximum entre ces deux entier est :\n%d",max);}
+else {
+  printf("les valeur sont identique :\n%d ");}
+
+}
+
+
+int main(){
+   int a,b;
+printf("saisir la valeur de a :\n");
+scanf("%d",&a);
+printf("saisir la valeur de b :\n");
+scanf("%d",&b);
+maximum(a,b) ;
+
+   return 0;
+}*/
+/*void minimum(int x, int y){                    //Challenge 4 : Fonction de Minimum
+
+
+ int min;
+
+ if (x>y){
+        min=y;
+    printf("le minimum entre ces deux entier est :\n%d",min);}
+else if (x<y){
+    min=x;
+    printf("le minimum entre ces deux entier est :\n%d",min);}
+else {
+  printf("les valeur sont identique :\n%d ");}
+
+}
+
+
+int main(){
+   int a,b;
+printf("saisir la valeur de a :\n");
+scanf("%d",&a);
+printf("saisir la valeur de b :\n");
+scanf("%d",&b);
+minimum(a,b) ;
+
+
+
+ return 0;
+}*/
+/*int factorielle(int x){           //Challenge 5 : Fonction de Factorielle
+
+int fact,i;
+fact=1;
+for ( i=1;i<=x;i++){
+  fact*=i;}
+  return fact;
+  }
+
+
+
+
+int main(){
+
+int a;
+printf("veuillez saisir une valeur positif: \n");
+scanf("%d",&a);
+if (a<0){
+    printf("veuillez saisir un nbr positif");
+}
+else {
+factorielle(a);
+printf("le factorielle de ce nombre est %d",factorielle(a));}
+
+return 0;
+}*/
+/*int verification(int n){                //Challenge 8 : Fonction de Vérification de Parité
+
+    if (n%2==0){
+        printf("le nbr est pair");
+        return 1;
+    }else {
+    printf("le nbr est impair");
+return 0;
+    }
+
+}
+
+int main(){
+int n;
+printf("veuillez saisir un nbr n:");
+scanf("%d",&n);
+verificatin (n) ;
+return 0;
+}*/
+
+int main(){
+
+return 0;
+}
